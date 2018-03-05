170f8022a96c9022238d58c76a6085e2092727e1
Sam Greene <s.greene@griffith.edu.au>
2018-03-05 13:04:55 +1000

    initial commit

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:silent_auth/silent_auth.dart';
+import 'package:idle_tracker/idle_tracker.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    return new Material(
+      color: Colors.blueAccent,
+    );
+  }
+}
